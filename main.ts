250cb26a31cd1860db3b5c7223a80ab4ed823631
mickeydarrenlau <darrenwjlau@gmail.com>
2024-02-11 22:28:25 +0800

    1.0.5 - Use a WSS Echo server to update all device

--- main.ts
+++ main.ts
@@ -4,10 +4,12 @@ import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Set
 
 interface ObisidianKVSettings {
 	kvdata: Object;
+	serverurl: string;
 }
 
 const DEFAULT_SETTINGS: ObisidianKVSettings = {
-	kvdata: {}
+	kvdata: {},
+	serverurl: "",
 }
 
 declare global {
@@ -28,7 +30,21 @@ class SharedStuff {
 		this.this2 = this2;
     }
 
+	async load() {
+		let data = await this.this2.loadSettings()
+		window.kv = new SharedStuff(data.kvdata, this.this2);
+		return window.kv;
+	}
+
     set(name: string, value: any) {
+		try {
+		if(this.this2.lastupdate == undefined) {
+		this.this2.lastupdate = Date.now();
+		this.this2.socket.send(JSON.stringify({type: "set", key: name, update: this.this2.lastupdate, value: value}));
+		} else {
+		this.this2.lastupdate = undefined
+		}
+	} catch (error) {}
         this.stuff[name] = value;
 		this.this2.saveSettings();
     }
@@ -38,6 +54,14 @@ class SharedStuff {
     }
 
     delete(name: string) {
+		try {
+		if(this.this2.lastupdate == undefined) {
+		this.this2.lastupdate = Date.now();
+		this.this2.socket.send(JSON.stringify({type: "delete", key: name, update: this.this2.lastupdate}));
+	} else {
+		this.this2.lastupdate = undefined
+		}
+	} catch (error) {}
         delete this.stuff[name];
 		this.this2.saveSettings();
     }
@@ -64,46 +88,72 @@ export default class ObisidianKV extends Plugin {
 	settings: ObisidianKVSettings
 	privatekv: SharedStuff
 	manifest: PluginManifest
+	socket: WebSocket
+	lastupdate: number
 
 	async onload() {
 		await this.loadSettings();
 		let this2 = this;
 		window.kv = new SharedStuff(this.settings.kvdata, this2);
-		this.privatekv = new SharedStuff({}, this2);
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 			this.addSettingTab(new ObisidianKVSettingTab(this.app, this));
-
-			let updateinterval = setInterval(async () => {
-				let currentdata = this.settings.kvdata
-				let newkvdata = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-				if(JSON.stringify(newkvdata.kvdata) == JSON.stringify(currentdata)) {
-					return;
-				} else {
-					await this.loadSettings();
-					window.kv = new SharedStuff(this.settings.kvdata, this2);
-					console.log("[ " + this.manifest.id +" ] Updated kv data");
+			let wssinit = () => {
+			if (!(this.settings.serverurl == '')) {
+				this.socket = new WebSocket(this.settings.serverurl);
+			} else {}
+			this.socket.onmessage = (event) => {
+				let data = JSON.parse(event.data);
+				if (data.update == this.lastupdate) {
+					return
 				}
-			}, 1000);
-
-			this.privatekv.set("updateinterval", updateinterval);
-			//clearInterval(this.privatekv.get("updateinterval"));
-
+				if (data.type == "set") {
+					window.kv.set(data.key, data.value);
+				} else if (data.type == "delete") {
+					window.kv.delete(data.key);
+				}
+	
+				
+	
+			}
+		}
 
 			
-
+			function attemptWssInit(delay = 1000) {
+				try {
+					wssinit();
+				} catch (error) {
+					setTimeout(() => {
+						attemptWssInit(delay);
+					}, delay);
+				}
+			}
+			
+			// Start the first attempt
+			attemptWssInit();
+			this.socket.onclose = () => {
+				console.log('WebSocket disconnected, attempting to reconnect...');
+				attemptWssInit();
+			};
+		
 		}
 
 		onunload() {
-			clearInterval(this.privatekv.get("updateinterval"));
+			(window.kv as any) = null;
+			this.socket.close();
 		}
 
 		async loadSettings() {
 			this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+			return this.settings;
 	}
 
 		async saveSettings() {
 			await this.saveData(this.settings);
+		}
+
+		onConfigFileChange(): void {
+			console.log("Config file changed");
 		}
 }
 
@@ -115,6 +165,7 @@ class ObisidianKVSettingTab extends PluginSettingTab {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
+
 
 	display(): void {
 		const {containerEl} = this;
@@ -134,6 +185,18 @@ class ObisidianKVSettingTab extends PluginSettingTab {
 				} catch (error) {
 					new Notice('Invalid JSON: ' + error.message, 5000);
 				}
+			}));
+
+		new Setting(containerEl)
+		.setName('Server URL')
+		.setDesc('This is the URL of the server to connect to. If you leave it blank, it will not connect to a server.')
+		.addText(text => text
+			.setPlaceholder('ws://localhost:8080')
+			.setValue(this.plugin.settings.serverurl)
+			.onChange(async (value) => {
+				this.plugin.settings.serverurl = value;
+				await this.plugin.saveSettings();
+				this.plugin.socket = new WebSocket(this.plugin.settings.serverurl);
 			}));
 		
 	}
